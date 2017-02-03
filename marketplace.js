cc638437a394d56ff2edc9a8b7ba29894e139d93
Peter Miklos <peter.miklos@gmail.com>
2017-02-03 08:45:46 +0000

    command matching is now regex based

--- marketplace.js
+++ marketplace.js
@@ -21,14 +21,14 @@ var KEYS_FILENAME = appDataDir + '/' + conf.KEYS_FILENAME;
 var wallet;
 
 
-function readKeys(onDone){
-	fs.readFile(KEYS_FILENAME, 'utf8', function(err, data){
-		if (err){
+function readKeys(onDone) {
+	fs.readFile(KEYS_FILENAME, 'utf8', function(err, data) {
+		if (err) {
 			console.log('failed to read keys, will gen');
 			var devicePrivKey = crypto.randomBytes(32);
 			var deviceTempPrivKey = crypto.randomBytes(32);
 			var devicePrevTempPrivKey = crypto.randomBytes(32);
-			writeKeys(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey, function(){
+			writeKeys(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey, function() {
 				onDone(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey);
 			});
 			return;
@@ -38,16 +38,16 @@ function readKeys(onDone){
 	});
 }
 
-function writeKeys(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey, onDone){
+function writeKeys(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey, onDone) {
 	var keys = {
 		permanent_priv_key: devicePrivKey.toString('base64'),
 		temp_priv_key: deviceTempPrivKey.toString('base64'),
 		prev_temp_priv_key: devicePrevTempPrivKey.toString('base64')
 	};
-	
+
 	console.log("Writing keys to " + KEYS_FILENAME);
-	
-	fs.writeFile(KEYS_FILENAME, JSON.stringify(keys), 'utf8', function(err){
+
+	fs.writeFile(KEYS_FILENAME, JSON.stringify(keys), 'utf8', function(err) {
 		if (err)
 			throw Error("failed to write keys file");
 		if (onDone)
@@ -55,36 +55,36 @@ function writeKeys(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey, onDo
 	});
 }
 
-function createNewSession(device_address, onDone){
-    console.log("Session started for " + device_address);
-    if (onDone)
-        onDone();
+function createNewSession(device_address, onDone) {
+	console.log("Session started for " + device_address);
+	if (onDone)
+		onDone();
 }
 
-function createWallet(onDone){
-	walletDefinedByKeys.createSinglesigWalletWithExternalPrivateKey(conf.xPubKey, conf.account, conf.homeDeviceAddress, function(_wallet){
+function createWallet(onDone) {
+	walletDefinedByKeys.createSinglesigWalletWithExternalPrivateKey(conf.xPubKey, conf.account, conf.homeDeviceAddress, function(_wallet) {
 		wallet = _wallet;
 		onDone();
 	});
 }
 
-function handleNoWallet(from_address){
+function handleNoWallet(from_address) {
 	if (from_address === conf.homeDeviceAddress && wallet === null)
-		createWallet(function(){
+		createWallet(function() {
 			device.sendMessageToDevice(from_address, 'text', "Wallet created, all new addresses will be synced to your device");
 		});
 	else
 		device.sendMessageToDevice(from_address, 'text', "The shop is not set up yet, try again later");
 }
 
-function replaceConsoleLog(){
+function replaceConsoleLog() {
 	var log_filename = conf.LOG_FILENAME || (appDataDir + '/log.txt');
 	var writeStream = fs.createWriteStream(log_filename);
 	console.log('---------------');
-	console.log('From this point, output will be redirected to '+log_filename);
+	console.log('From this point, output will be redirected to ' + log_filename);
 	console.log("To release the terminal, type Ctrl-Z, then 'bg'");
-	console.log = function(){
-		writeStream.write(Date().toString()+': ');
+	console.log = function() {
+		writeStream.write(Date().toString() + ': ');
 		writeStream.write(util.format.apply(null, arguments) + '\n');
 	};
 	console.warn = console.log;
@@ -93,25 +93,24 @@ function replaceConsoleLog(){
 
 
 model.connect(function() {
-   console.log("connected to mongodb");
+	console.log("connected to mongodb");
 });
 
-process.on('SIGINT', function() {  
-	model.connection.close(function () { 
-		console.log('Mongoose default connection disconnected through app termination'); 
-		process.exit(0); 
-	}); 
+process.on('SIGINT', function() {
+	model.connection.close(function() {
+		console.log('Mongoose default connection disconnected through app termination');
+		process.exit(0);
+	});
 });
 
 if (!conf.permanent_paring_secret)
 	throw Error('no conf.permanent_paring_secret');
 
 db.query(
-	"INSERT "+db.getIgnore()+" INTO pairing_secrets (pairing_secret, expiry_date, is_permanent) VALUES(?, '2035-01-01', 1)", 
-	[conf.permanent_paring_secret]
+	"INSERT " + db.getIgnore() + " INTO pairing_secrets (pairing_secret, expiry_date, is_permanent) VALUES(?, '2035-01-01', 1)", [conf.permanent_paring_secret]
 );
 
-db.query("SELECT wallet FROM wallets", function(rows){
+db.query("SELECT wallet FROM wallets", function(rows) {
 	if (rows.length > 1)
 		throw Error('more than 1 wallet');
 	if (rows.length === 1)
@@ -120,8 +119,8 @@ db.query("SELECT wallet FROM wallets", function(rows){
 		wallet = null; // different from undefined
 });
 
-readKeys(function(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey){
-	var saveTempKeys = function(new_temp_key, new_prev_temp_key, onDone){
+readKeys(function(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey) {
+	var saveTempKeys = function(new_temp_key, new_prev_temp_key, onDone) {
 		writeKeys(devicePrivKey, new_temp_key, new_prev_temp_key, onDone);
 	};
 	device.setDevicePrivateKey(devicePrivKey);
@@ -129,28 +128,33 @@ readKeys(function(devicePrivKey, deviceTempPrivKey, devicePrevTempPrivKey){
 	device.setDeviceName(conf.deviceName);
 	device.setDeviceHub(conf.hub);
 	var my_device_pubkey = device.getMyDevicePubKey();
-	console.log("my device pubkey: "+my_device_pubkey);
-	console.log("my pairing code: "+my_device_pubkey+"@"+conf.hub+"#"+conf.permanent_paring_secret);
-	
+	console.log("my device pubkey: " + my_device_pubkey);
+	console.log("my pairing code: " + my_device_pubkey + "@" + conf.hub + "#" + conf.permanent_paring_secret);
+
 	replaceConsoleLog();
 });
 
-eventBus.on('paired', function(from_address){
+eventBus.on('paired', function(from_address) {
 	if (!wallet)
 		return handleNoWallet(from_address);
 
-	createNewSession(from_address, function(){
-	    model.Account.find({device: from_address}, function(error, accounts) {
-	        if (accounts.length == 0) {
-		        device.sendMessageToDevice(from_address, 'text', "Hi! Welcome to the Marketplace. Type [help](command:help) for available commands.");
-	            var account = new model.Account({device: from_address});
-	            account.save(function(err, account) {
-                    if (err) return console.error(err);
-                });
-	        } else {
-    		    device.sendMessageToDevice(from_address, 'text', "Hi! Welcome back! Type [help](command:help) for available commands.");
-	        }
-	    });
+	createNewSession(from_address, function() {
+		model.Account.find({
+			device: from_address
+		}, function(error, accounts) {
+			if (accounts.length == 0) {
+				device.sendMessageToDevice(from_address, 'text', "Hi! Welcome to the Marketplace. Type [help](command:help) for available commands.");
+				var account = new model.Account({
+					device: from_address
+				});
+				account.save(function(err, account) {
+					if (err) return console.error(err);
+				});
+			}
+			else {
+				device.sendMessageToDevice(from_address, 'text', "Hi! Welcome back! Type [help](command:help) for available commands.");
+			}
+		});
 	});
 });
 
@@ -161,74 +165,78 @@ const usage = `Use the commands below:
 [search](command:search) - search for items on sale
 `;
 
-chat.when("my items", seller.myItems);
-chat.when("add", seller.add);
-chat.when("remove", seller.remove);
-chat.when("edit", seller.editItem);
-chat.when("search", buyer.search);
-chat.when("item", buyer.browseItem);
-chat.when("seller", buyer.browseSeller);
+chat.when(/^my items$/, seller.myItems);
+chat.when(/^add$/, seller.add);
+chat.when(/^add (.*)$/, seller.add);
+chat.when(/^remove ([0-9a-f]+)$/, seller.remove);
+chat.when(/^edit ([0-9a-f]+)$/, seller.editItem);
+chat.when(/^set title$/, seller.setTitleNoArgs);
+chat.when(/^set title (.*)$/, seller.setTitle);
+chat.when(/^search (.*)$/, buyer.search);
+chat.when(/^item ([0-9a-f]+)$/, buyer.browseItem);
+chat.when(/^seller ([0-9a-f]+)$/, buyer.browseSeller);
 
-chat.when("help", function(reply, message) {
-    reply(usage);
+chat.when(/^help$/, function(reply, message) {
+	reply(usage);
 });
 
-chat.when("nomatch", function(reply, message) {
+chat.nomatch(function(reply, message) {
 	reply("Not sure how to help with that. Try [help](command:help) for available commands.");
 });
 
 eventBus.on("text", function(from_address, text) {
 	if (!wallet) return handleNoWallet(from_address);
-	
-	model.Account.findOne({"device": from_address}, function(err, account) {
-        if (err) {
-        	console.error("Failed to access account: " + err);
-            device.sendMessageToDevice(from_address, "text", "Your account cannot be accessed currently, please try again later.");
-            return;
-        }
-        
-        if (account == null) {
-        	console.error("Account not found for device: " + from_address);
-            device.sendMessageToDevice(from_address, "text", "Account does not exist for this device. Pair your device to create one");
-        	return;
-        }
 
-        console.log("[" + account._id +"] Text received: '" + text + "'"); 
+	model.Account.findOne({
+		"device": from_address
+	}, function(err, account) {
+		if (err) {
+			console.error("Failed to access account: " + err);
+			device.sendMessageToDevice(from_address, "text", "Your account cannot be accessed currently, please try again later.");
+			return;
+		}
 
-        let args = text.trim().split(" ");
-	        
-    	if (Array.isArray(account.conversation) && account.conversation.length > 0) {
-    		let lastConversation = account.conversation.pop();
-    		lastConversation.remove();
-    		account.markModified("conversation");
+		if (account == null) {
+			console.error("Account not found for device: " + from_address);
+			device.sendMessageToDevice(from_address, "text", "Account does not exist for this device. Pair your device to create one");
+			return;
+		}
 
-    		account.save(function(err) {
-    			if (err) {
-    				console.error("[" + account._id +"] Failed to resume conversation: " + err);
-                	device.sendMessageToDevice(from_address, "text", "Something went wrong, please try again.");
-    				return;
-    			}
-    			
-    			let message = { command: lastConversation.action, args: args, account: account};
-    			
-		    	chat.receive(message, function(response) {
-		    		device.sendMessageToDevice(from_address, "text", response);
-		    	});
-    		});
-    	} else {
-	        let command = args.shift().toLowerCase();
-	        
-	        if (command == "my" && args.length > 0) {
-	            command += " " + args.shift().toLowerCase();
-	        }
-	        
-	        let message = { command: command, args: args, account: account};
-	        
-	        chat.receive(message, function(response) {
+		console.log("[" + account._id + "] Text received: '" + text + "'");
+
+		if (Array.isArray(account.conversation) && account.conversation.length > 0) {
+			let lastConversation = account.conversation.pop();
+			lastConversation.remove();
+			account.markModified("conversation");
+
+			account.save(function(err) {
+				if (err) {
+					console.error("[" + account._id + "] Failed to resume conversation: " + err);
+					device.sendMessageToDevice(from_address, "text", "Something went wrong, please try again.");
+					return;
+				}
+
+				let message = {
+					text: lastConversation.action + " " + text.trim(),
+					account: account
+				};
+
+				chat.receive(message, function(response) {
+					device.sendMessageToDevice(from_address, "text", response);
+				});
+			});
+		}
+		else {
+			let message = {
+				text: text.trim(),
+				account: account
+			};
+
+			chat.receive(message, function(response) {
 				device.sendMessageToDevice(from_address, "text", response);
-	    	});
-    	}
+			});
+		}
 
-    });
+	});
 
 });
--- test/chatTest.js
+++ test/chatTest.js
@@ -4,21 +4,25 @@
 const assert = require('assert');
 const chat = require("../lib/chat.js");
 
-chat.when('test', function(reply, message) {
-    reply("echo: " + message.command);
+chat.when(/say (.*)/, function(reply, message) {
+    reply(message.args[0]);
 });
 
-chat.when('nomatch', function(reply, message) {
+chat.nomatch(function(reply, message) {
     reply("no match found");
 });
 
 
-chat.receive({command:"test"}, function(text) {
-    assert.equal(text, "echo: test");
+chat.receive({
+    text: "say test"
+}, function(response) {
+    assert.equal(response, "test");
 });
 
-chat.receive({command:"blabla"}, function(text) {
-    assert.equal(text, "no match found");
+chat.receive({
+    text: "blabla"
+}, function(response) {
+    assert.equal(response, "no match found");
 });
 
 process.exit(0);
